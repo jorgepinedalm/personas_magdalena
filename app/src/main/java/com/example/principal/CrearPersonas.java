8cb666aa565562e606fe0e67d87e1225dd2a7bff
Jorge <jpineda@unimagdalena.edu.co>
2019-07-12 19:29:40 -0500

    Interfaz del principal y el agregar terminadas

---
+++ app/src/main/java/com/example/principal/CrearPersonas.java
@@ -0,0 +1,14 @@
+package com.example.principal;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class CrearPersonas extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_crear_personas);
+    }
+}
